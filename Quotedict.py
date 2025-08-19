7f06d91eefc40ac85c073d709ec0547c57a7c7bf
acurldiem <154383731+acurldiem@users.noreply.github.com>
2025-08-19 18:59:51 -0400

    Create Quotedict.py
    quotes for banners, emails, community page and such

---
+++ Quotedict.py
@@ -0,0 +1,174 @@
+##Python quote dictionary 
+
+quotes = [
+    {
+        "quote": “Communication is merely an exchange of information, but connection is an exchange of our humanity.” 
+        "author": "Sean Stephenson”
+    },
+    {
+        "quote": “Perhaps the secret of living well is not in having all the answers but in pursuing unanswerable questions in good company.” 
+        "author": "Rachel Naomi Remen”
+    },
+    {
+       "quote": “When one tugs at a single thing in nature, he finds it attached to the rest of the world.” 
+        "author": "John Muir”
+    },
+    {
+        "quote": “In nature we never see anything isolated, but everything in connection with something else which is before it, beside it, under it and over it.” 
+        "author": "Johann Wolfgang von Goethe”
+    },
+    {
+        "quote": “Eventually everything connects – people, ideas, objects. The quality of the connections is the key to quality per se.” 
+        "author": "Charles Eames”
+    },
+    {
+        "quote": “I am a part of all that I have met.” 
+        "author": "Lord Tennyson”
+    },
+    {
+        "quote": “A thousand fibers connect us with our fellow men. Our actions run as causes, and they come back to us as effects.” 
+        "author": "Herman Melville”
+    },
+    {
+        “Relationships are all there is. Everything in the universe only exists because it is in relationship to everything else. Nothing exists in isolation. We have to stop pretending we are individuals that can go it alone. ” 
+        "author": "Margaret J. Wheatley”
+    },
+    {
+        "quote": “Whatever affects one directly, affects all indirectly. I can never be what I ought to be until you are what you ought to be. This is the interrelated structure of reality.” 
+        "author": "Martin Luther King Jr.”
+    },
+    {
+       "quote": “Friendship is based on the oldest and most intrinsic human awareness that there is more to life than just ourselves.” 
+        "author": "Christopher Hansard”
+    },
+    {
+        "quote": “Cherish your human connections: your relationships with friends and family.” 
+        "author": "Joseph Brodsky”
+    },
+    {
+       "quote": “Healing yourself is connected with healing others.” 
+        "author": "Yoko Ono”
+    },
+    {
+        "quote": "Know me for my abilities, not my disability.",
+        "author": "Robert M. Hensel"
+    },
+    {
+        "quote": "The only disability in life is a bad attitude.",
+        "author": "Scott Hamilton"
+    },
+    {
+        "quote": "Beauty is in the heart of the beholder.",
+        "author": "H. G. Wells"
+    },
+    {
+        "quote": "Everything has beauty, but not everyone sees it.",
+        "author": "Confucius"
+    },
+    {
+        "quote": "Disability is not a brave struggle or ‘courage in the face of adversity.’ Disability is an art. It’s an ingenious way to live.",
+        "author": "Neil Marcus"
+    },
+    {
+        "quote": "I choose not to place ‘DIS,’ in my ability.",
+        "author": "Robert M. Hensel"
+    },
+    {
+        "quote": "Being disabled should not mean being disqualified from having access to every aspect of life.",
+        "author": "Emma Thompson"
+    },
+    {
+        "quote": "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+        "author": "Helen Keller"
+    },
+    {
+        "quote": "Just because a man lacks the use of his eyes doesn’t mean he lacks vision.",
+        "author": "Stevie Wonder"
+    },
+    {
+        "quote": "My disability has opened my eyes to see my true abilities.",
+        "author": "Samuel P. King"
+    },
+    {
+        "quote": "People with disabilities are the world’s largest minority, yet we are the most resilient and creative group in society.",
+        "author": "Marlee Matlin"
+    },
+    {
+        "quote": "Abled does not mean enabled. Disabled does not mean less abled.",
+        "author": "Khang Kijarro Nguyen"
+    },
+    {
+        "quote": "A hero is an ordinary individual who finds the strength to persevere and endure in spite of overwhelming obstacles.",
+        "author": "Christopher Reeve"
+    },
+    {
+        "quote": "Never bend your head. Always hold it high. Look the world straight in the eye.",
+        "author": "Helen Keller"
+    },
+    {
+        "quote": "However difficult life may seem, there is always something you can do and succeed at.",
+        "author": "Stephen Hawking"
+    },
+    {
+        "quote": "Ability is what you’re capable of doing. Motivation determines what you do. Attitude determines how well you do it.",
+        "author": "Lou Holtz"
+    },
+    {
+        "quote": "I don’t need easy. I just need possible.",
+        "author": "Bethany Hamilton"
+    },
+    {
+        "quote": "The greater the obstacle, the more glory in overcoming it.",
+        "author": "Molière"
+    },
+    {
+        "quote": "It always seems impossible until it’s done.",
+        "author": "Nelson Mandela"
+    },
+    {
+        "quote": "Difficulties strengthen the mind, as labor does the body.",
+        "author": "Seneca"
+    },
+    {
+        "quote": "Your present circumstances don’t determine where you can go; they merely determine where you start.",
+        "author": "Nido Qubein"
+    },
+    {
+        "quote": "Disability is a matter of perception. If you can do just one thing well, you’re needed by someone.",
+        "author": "Martina Navratilova"
+    },
+    {
+        "quote": "Love does not dominate; it cultivates.",
+        "author": "Johann Wolfgang von Goethe"
+    },
+    {
+        "quote": "The most precious gift we can offer others is our presence.”,
+        "author": None
+    },
+    {
+        "quote": "It's always better when we're together.",
+        "author": "Jack Johnson"
+    },
+    {
+        "quote": "A best friend is like a four-leaf clover: hard to find and lucky to have.",
+        "author": None
+    },
+    {
+        "quote": "In your friendship, I found the love I never knew I was missing.",
+        "author": None
+    },
+    {
+        "quote": "Friendship is the soil from which the most beautiful love grows.",
+        "author": None
+    },
+    {
+        "quote": "Love and friendship are two sides of the same beautiful coin.",
+        "author": None
+    },
+    {
+        "quote": "The best kind of love is the one that started as the best kind of friendship.",
+        "author": None
+    }
+]
+
+
